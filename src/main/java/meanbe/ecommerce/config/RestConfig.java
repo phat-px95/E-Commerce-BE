455b2a3f858d97ea43fc2c21f332f01aa0975c19
XuanPhat <phatpx32@wru.vn>
2024-05-20 11:01:36 -0400

    Add configuration to not allow PUT POST DELETE for now

---
+++ src/main/java/meanbe/ecommerce/config/RestConfig.java
@@ -0,0 +1,29 @@
+package meanbe.ecommerce.config;
+
+import meanbe.ecommerce.entity.Product;
+import meanbe.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unSupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unSupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unSupportedActions));
+    }
+
+}
