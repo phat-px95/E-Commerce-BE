70f7ce9e797e6c09fd4e65ffe068d75829f4f175
XuanPhat <phatpx32@wru.vn>
2024-05-29 21:55:12 -0400

    Search By Category Id

--- src/main/java/meanbe/ecommerce/dao/ProductRepository.java
+++ src/main/java/meanbe/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package meanbe.ecommerce.dao;
 
 import meanbe.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") long id, Pageable pageable);
 
 }
